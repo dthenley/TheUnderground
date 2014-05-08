dce2af40a4693eb637a514766015345e8be2fabf
Donte <donteturner@gmail.com>
2014-05-08 01:35:08 -0500

    media player integration

--- sitepage/js/site.js
+++ sitepage/js/site.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
       $("#jquery_jplayer_1").jPlayer({
         ready: function () {
           $(this).jPlayer("setMedia", {
-            title: "Bonfire",
+            title: "<h2>Childish Gambino</h2><p>Bonfire</p>",
             m4a: "audio/bonfire.mp3",
             oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
           });
