2646c9567c5b5f6b693d1c644434f1a7f58cdb05
Donte <donteturner@gmail.com>
2014-05-14 20:26:22 -0500

    custom media player

--- sitepage/js/site.js
+++ sitepage/js/site.js
@@ -9,21 +9,30 @@
         },
         swfPath: "/js",
         supplied: "m4a, oga"
-      });
-    
-    $("#jquery_jplayer_2").jPlayer({
-        ready: function () {
-          $(this).jPlayer("setMedia", {
-            title: "<h2>Childish Gambino</h2><p>L.E.S</p>",
-            m4a: "audio/LES.mp3",
-            oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
-          });
-        },
-        swfPath: "/js",
-        supplied: "m4a, oga"
-      });
-    });*/
-  $("#myPlayButton").click( function() {
-     $("#jquery_jplayer_1").jPlayer("play");
-     
-
+      });*/
+ $(document).ready(function(){
+  $("#jquery_jplayer_1").jPlayer({
+   ready: function () {
+    $(this).jPlayer("setMedia", {
+      title: "<h2>Childish Gambino</h2><p>Bonfire</p>",
+      m4a: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg",
+      oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+    });
+   },
+   swfPath: "/js",
+   supplied: "m4a, oga"
+  });
+ });
+  $(document).ready(function(){
+  $("#jquery_jplayer_2").jPlayer({
+   ready: function () {
+    $(this).jPlayer("setMedia", {
+      title: "<h2>Childish Gambino</h2><p>Bonfire</p>",
+      m4a: "audio/Bonfire.mp3",
+      oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+    });
+   },
+   swfPath: "/js",
+   supplied: "m4a, oga"
+  });
+ });
